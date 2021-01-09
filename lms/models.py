71389074bffa3eebffa9c847573dceb3627acd70
el-rizado <amirhosseinmohseni1996@yahoo.com>
2021-01-09 12:13:33 +0330

    cmoplete time and date models

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Day(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Time(models.Model):
+    first_start = '07:30'
+    second_start = '09:00'
+    third_start = '10:30'
+    forth_start = '14:00'
+    fifth_start = '16:00'
+    starts = {
+        (first_start, '07:30'), (second_start, '09:00'), (third_start, '10:30'),
+        (forth_start, '14:00'), (fifth_start, '16:00')
+    }
+    first_end = '09:00'
+    second_end = '10:30'
+    third_end = '12:00'
+    forth_end = '15:30'
+    fifth_end = '17:30'
+    ends = {
+        (first_end, '09:00'), (second_end, '10:30'), (third_end, '12:00'),
+        (forth_end, '15:30'), (fifth_end, '17:30')
+    }
+    start_time = models.CharField(max_length=5, choices=starts, default=first_start)
+    end_time = models.CharField(max_length=5, choices=ends, default=first_end)
+
+    def __str__(self):
+        return str(self.start_time) + ' - ' + str(self.end_time)
+
+
+
+
+
--- university/settings.py
+++ university/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lms.apps.LmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'university.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'university',
+        'USER': 'postgres',
+        'PASSWORD': '1607',
+        'HOST': 'localhost',
+
     }
 }
 
